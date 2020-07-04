89d278eeed06c6969849764e4f6a7c4cafd66caa
jneo8 <jneo8lin@gmail.com>
2020-07-05 01:27:20 +0800

    Move insert db && output to func

--- pbf_parser.go
+++ pbf_parser.go
@@ -6,7 +6,6 @@ import (
 	"github.com/syndtr/goleveldb/leveldb"
 	"github.com/syndtr/goleveldb/leveldb/opt"
 	"github.com/thomersch/gosmparse"
-	"io"
 	"os"
 	"strconv"
 	"sync"
@@ -22,13 +21,16 @@ type pbfParser struct {
 	Args  Args
 
 	// Log
-	Logger      *log.Logger
-	elementChan chan Element
+	Logger    *log.Logger
+	elementCh chan Element
 
 	Error error
 
 	// Report
 	Report Report
+
+	// output
+	OutputCh chan Element
 }
 
 func (p *pbfParser) Err() error {
@@ -41,10 +43,10 @@ func (p *pbfParser) Close() error {
 
 // Run ...
 func (p *pbfParser) Iterator() <-chan Element {
-	outputCh := make(chan Element)
+	p.OutputCh = make(chan Element)
 
 	go func() {
-		defer close(outputCh)
+		defer close(p.OutputCh)
 		st := time.Now()
 
 		// counter
@@ -60,21 +62,21 @@ func (p *pbfParser) Iterator() <-chan Element {
 		p.Logger.Info(nodeC, wayC, relationC)
 		totalElement := nodeC + wayC + relationC
 
-		// reader .
+		// Get file size
+
 		reader, err := os.Open(p.Args.PBFFile)
 		if err != nil {
 			p.Error = err
 			return
 		}
 		defer reader.Close()
-
-		// Get file size
 		fInfo, err := reader.Stat()
 		if err != nil {
 			p.Error = err
 			return
 		}
 		p.Report.Fizesize = fInfo.Size()
+		reader.Close()
 
 		db, err := leveldb.OpenFile(
 			p.Args.LevelDBPath,
@@ -108,159 +110,32 @@ func (p *pbfParser) Iterator() <-chan Element {
 		}
 		p.Logger.Info("Finish index")
 
-		// InsertDB
-		// Put way refs, relation member into db.
-		p.Batch = leveldb.MakeBatch(p.Args.BatchSize)
-		defer p.Batch.Reset()
-
-		p.elementChan = make(chan Element)
-
-		insertDBWg := sync.WaitGroup{}
-		insertDBWg.Add(1)
-		insertDBBar := newBar(totalElement, "InsertDB")
-		go func() {
-			defer insertDBWg.Done()
-			defer insertDBBar.Finish()
-			idx := 0
-			for emt := range p.elementChan {
-				func() {
-					defer insertDBBar.Increment()
-					idx++
-					// Check batch size every batchsize.
-					if idx%p.Args.BatchSize == 0 {
-						if err := p.checkBatch(); err != nil {
-							p.Error = err
-						}
-					}
-					switch emt.Type {
-					case 0:
-						if p.PBFMasks.WayRefs.Has(emt.Node.ID) || p.PBFMasks.RelNodes.Has(emt.Node.ID) {
-							id, b := nodeToBytes(emt.Node)
-							p.Batch.Put(
-								[]byte(id),
-								b,
-							)
-						}
-					case 1:
-						if p.PBFMasks.RelWays.Has(emt.Way.ID) {
-							emtBytes, err := emt.ToBytes()
-							if err != nil {
-								p.Report.FatalWay++
-								return
-							}
-							p.Batch.Put(
-								[]byte("W"+strconv.FormatInt(emt.Way.ID, 10)),
-								emtBytes,
-							)
-						}
-					case 2:
-						if p.PBFMasks.RelRelation.Has(emt.Relation.ID) {
-							emtBytes, err := emt.ToBytes()
-							if err != nil {
-								p.Report.FatalRelation++
-								return
-							}
-							p.Batch.Put(
-								[]byte("R"+strconv.FormatInt(emt.Relation.ID, 10)),
-								emtBytes,
-							)
-						}
-					}
-
-				}()
-			}
-		}()
-
-		firstRoundDecoder := gosmparse.NewDecoder(reader)
-		if err := firstRoundDecoder.Parse(p); err != nil {
-			p.Error = err
-			return
-		}
-		close(p.elementChan)
-		insertDBWg.Wait()
-
-		// Flush batch.
-		if err := p.flushBatch(true); err != nil {
+		// First round
+		// Insert element to leveldb
+		if err := p.runInserter(totalElement); err != nil {
 			p.Error = err
 			return
 		}
 		p.Logger.Info("Finish insert db")
 
-		// Rewind pbf file reader
-		if _, err := reader.Seek(0, io.SeekStart); err != nil {
-			p.Error = err
-			return
-		}
-
 		// Final round.
 		// Output element
-
-		// Re-init element chan
-		p.elementChan = make(chan Element)
-
-		finalRWg := sync.WaitGroup{}
-		finalRWg.Add(1)
-		finalRBar := newBar(totalElement, "Output")
-		go func() {
-			defer finalRWg.Done()
-			defer finalRBar.Finish()
-			for emt := range p.elementChan {
-				func() {
-					defer finalRBar.Increment()
-					switch emt.Type {
-					case 0:
-						if p.PBFMasks.Nodes.Has(emt.Node.ID) {
-							p.Report.ProcessedNode++
-							outputCh <- emt
-						}
-					case 1:
-						if p.PBFMasks.Ways.Has(emt.Way.ID) {
-							emts, err := p.dbLookupWayEmts(&emt.Way)
-							if err != nil {
-								p.Logger.Warning(err)
-								p.Report.FatalWay++
-								return
-							}
-							emt.Elements = emts
-							p.Report.ProcessedWay++
-							outputCh <- emt
-						}
-
-					case 2:
-						if p.PBFMasks.Relations.Has(emt.Relation.ID) {
-							emts, err := p.dbLookupRelationEmts(&emt.Relation, []int64{})
-							if err != nil {
-								// p.Logger.Warning(err)
-								p.Report.FatalRelation++
-								return
-							}
-							emt.Elements = emts
-							p.Report.ProcessedRelation++
-							outputCh <- emt
-						}
-					}
-
-				}()
-			}
-		}()
-		decoder := gosmparse.NewDecoder(reader)
-		if err := decoder.Parse(p); err != nil {
+		if err := p.runOutputer(totalElement); err != nil {
 			p.Error = err
 			return
 		}
-		close(p.elementChan)
-		finalRWg.Wait()
+		p.Logger.Info("Finish output")
 
 		// Report
 		p.Report.SpendTime = time.Since(st)
 		p.Logger.Infof(p.Report.GetReport())
 	}()
-	return outputCh
+	return p.OutputCh
 }
 
 // ReadNode ...
 func (p *pbfParser) ReadNode(node gosmparse.Node) {
-	p.elementChan <- Element{
+	p.elementCh <- Element{
 		Type: 0,
 		Node: node,
 	}
@@ -268,7 +143,7 @@ func (p *pbfParser) ReadNode(node gosmparse.Node) {
 
 // ReadWay ...
 func (p *pbfParser) ReadWay(way gosmparse.Way) {
-	p.elementChan <- Element{
+	p.elementCh <- Element{
 		Type: 1,
 		Way:  way,
 	}
@@ -276,7 +151,7 @@ func (p *pbfParser) ReadWay(way gosmparse.Way) {
 
 // ReadRelation ...
 func (p *pbfParser) ReadRelation(relation gosmparse.Relation) {
-	p.elementChan <- Element{
+	p.elementCh <- Element{
 		Type:     2,
 		Relation: relation,
 	}
@@ -285,6 +160,157 @@ func (p *pbfParser) ReadRelation(relation gosmparse.Relation) {
 // SetLogger ...
 func (p *pbfParser) SetLogger(logger *log.Logger) {
 	p.Logger = logger
+}
+
+// InsertDB, Put way refs, relation member into db.
+func (p *pbfParser) runInserter(totalElement int) error {
+
+	// reader .
+	reader, err := os.Open(p.Args.PBFFile)
+	if err != nil {
+		return err
+	}
+	defer reader.Close()
+
+	p.Batch = leveldb.MakeBatch(p.Args.BatchSize)
+	defer p.Batch.Reset()
+
+	p.elementCh = make(chan Element)
+
+	insertDBWg := sync.WaitGroup{}
+	insertDBWg.Add(1)
+	insertDBBar := newBar(totalElement, "InsertDB")
+	go func() {
+		defer insertDBWg.Done()
+		defer insertDBBar.Finish()
+		idx := 0
+		for emt := range p.elementCh {
+			func() {
+				defer insertDBBar.Increment()
+				idx++
+				// Check batch size every batchsize.
+				if idx%p.Args.BatchSize == 0 {
+					if err := p.checkBatch(); err != nil {
+						p.Error = err
+					}
+				}
+				switch emt.Type {
+				case 0:
+					if p.PBFMasks.WayRefs.Has(emt.Node.ID) || p.PBFMasks.RelNodes.Has(emt.Node.ID) {
+						id, b := nodeToBytes(emt.Node)
+						p.Batch.Put(
+							[]byte(id),
+							b,
+						)
+					}
+				case 1:
+					if p.PBFMasks.RelWays.Has(emt.Way.ID) {
+						emtBytes, err := emt.ToBytes()
+						if err != nil {
+							p.Report.FatalWay++
+							return
+						}
+						p.Batch.Put(
+							[]byte("W"+strconv.FormatInt(emt.Way.ID, 10)),
+							emtBytes,
+						)
+					}
+				case 2:
+					if p.PBFMasks.RelRelation.Has(emt.Relation.ID) {
+						emtBytes, err := emt.ToBytes()
+						if err != nil {
+							p.Report.FatalRelation++
+							return
+						}
+						p.Batch.Put(
+							[]byte("R"+strconv.FormatInt(emt.Relation.ID, 10)),
+							emtBytes,
+						)
+					}
+				}
+
+			}()
+		}
+	}()
+
+	firstRoundDecoder := gosmparse.NewDecoder(reader)
+	if err := firstRoundDecoder.Parse(p); err != nil {
+		return err
+	}
+	close(p.elementCh)
+	insertDBWg.Wait()
+
+	// Flush batch.
+	if err := p.flushBatch(true); err != nil {
+		return err
+	}
+	p.Logger.Info("Finish insert db")
+	return nil
+}
+
+func (p *pbfParser) runOutputer(totalElement int) error {
+
+	reader, err := os.Open(p.Args.PBFFile)
+	if err != nil {
+		return err
+	}
+	defer reader.Close()
+
+	// Init element chan
+	p.elementCh = make(chan Element)
+
+	finalRWg := sync.WaitGroup{}
+	finalRWg.Add(1)
+	finalRBar := newBar(totalElement, "Output")
+	go func() {
+		defer finalRWg.Done()
+		defer finalRBar.Finish()
+		for emt := range p.elementCh {
+			func() {
+				defer finalRBar.Increment()
+				switch emt.Type {
+				case 0:
+					if p.PBFMasks.Nodes.Has(emt.Node.ID) {
+						p.Report.ProcessedNode++
+						p.OutputCh <- emt
+					}
+				case 1:
+					if p.PBFMasks.Ways.Has(emt.Way.ID) {
+						emts, err := p.dbLookupWayEmts(&emt.Way)
+						if err != nil {
+							p.Logger.Warning(err)
+							p.Report.FatalWay++
+							return
+						}
+						emt.Elements = emts
+						p.Report.ProcessedWay++
+						p.OutputCh <- emt
+					}
+
+				case 2:
+					if p.PBFMasks.Relations.Has(emt.Relation.ID) {
+						emts, err := p.dbLookupRelationEmts(&emt.Relation, []int64{})
+						if err != nil {
+							// p.Logger.Warning(err)
+							p.Report.FatalRelation++
+							return
+						}
+						emt.Elements = emts
+						p.Report.ProcessedRelation++
+						p.OutputCh <- emt
+					}
+				}
+
+			}()
+		}
+	}()
+	decoder := gosmparse.NewDecoder(reader)
+	if err := decoder.Parse(p); err != nil {
+		return err
+	}
+	close(p.elementCh)
+	finalRWg.Wait()
+	return nil
 }
 
 func (p *pbfParser) checkBatch() error {
