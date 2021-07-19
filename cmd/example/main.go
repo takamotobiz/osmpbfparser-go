24336d8a76ee1dcef2b1dfd7be1f27bf0da81712
takamotobiz <keiji.takamoto@takamoto.biz>
2021-07-19 20:11:30 +0900

    amenity/schoolのみ抽出

--- cmd/example/main.go
+++ cmd/example/main.go
@@ -2,26 +2,49 @@ package main
 
 import (
 	"fmt"
-	"github.com/jneo8/osmpbfparser-go"
 	"log"
+
+	"github.com/jneo8/osmpbfparser-go"
 )
 
 func main() {
 	parser := osmpbfparser.New(
 		osmpbfparser.Args{
-			PBFFile:     "./static/test.pbf",
+			PBFFile:     "/Users/takamotokeiji/Downloads/shikoku-latest.osm.pbf",
 			LevelDBPath: "/tmp/osmpbfparser",
 			BatchSize:   10000,
 		},
 	)
+	var nc, wc, rc uint64
 
 	for emt := range parser.Iterator() {
-		rawJSON, err := emt.ToGeoJSON()
-		if err != nil {
-			log.Fatal(err)
+		// rawJSON, err := emt.ToGeoJSON()
+		// if err != nil {
+		//     log.Fatal(err)
+		// }
+		// fmt.Println(string(rawJSON))
+
+		tags := emt.GetTags()
+		if nv, fl := tags["amenity"]; fl == true {
+			if nv == "school" {
+				rawJSON, err := emt.ToGeoJSON()
+				if err != nil {
+					log.Fatal(err)
+				}
+				fmt.Println(string(rawJSON))
+				switch emt.Type {
+				case 0: //Node
+					nc++
+				case 1: //Way
+					wc++
+				case 2: //Relation
+					rc++
+				}
+			}
 		}
-		fmt.Println(string(rawJSON))
 	}
+	fmt.Printf("Nodes: %d, Ways: %d, Relations: %d\n", nc, wc, rc)
+
 	if err := parser.Err(); err != nil {
 		log.Fatal(err)
 	}
